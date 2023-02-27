38e1a779b4bd099c929e3c944392d0a4ff4615e1
suzu784 <s.suzuki784@hotmail.com>
2023-02-27 22:59:24 +0900

    create login/logout

--- resources/views/login/login_form.blade.php
+++ resources/views/login/login_form.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,29 +11,26 @@
   {{-- Styles --}}
   <link rel="stylesheet" href="{{asset('css/signin.css')}}">
 </head>
+
 <body>
   <form class="form-signin" method="POST" action="{{route('login')}}">
     @csrf
     <h1 class="h3 mb-3 font-weight-normal">ログインフォーム</h1>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-        @if(session('login_error'))
-        <div class="alert alert-danger">
-          {{session('login_error')}}
-        </div>
-        @endif
-      </ul>
-    </div>
-    @endif
+    @foreach ($errors->all() as $error)
+    <ul>
+      <li>{{ $error }}</li>
+    </ul>
+    @endforeach
+
+    <x-alert type="danger" :session="session('danger')" />
+
     <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
+    <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required
+      autofocus>
     <label for="inputPassword" class="sr-only">Password</label>
     <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
     <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
   </form>
 </body>
+
 </html>
